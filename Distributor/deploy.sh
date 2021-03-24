9efd9a987d2a684ee358dbd72846917ddcf2ffd0
Lewis Westbury <lewis@cantab.net>
2021-03-24 21:05:33 +0000

    Update deploy

--- Distributor/deploy.sh
+++ Distributor/deploy.sh
@@ -2,5 +2,5 @@
 
 pushd DistributeSocialLambda
 dotnet build
-dotnet lambda deploy-function --profile sa-social-distributor DistributeSocialLambda --function-role role-social-distributor
+dotnet lambda deploy-function --profile sa-social-distributor DistributeSocialLambda --function-role role-social-distributor --region eu-west-2
 popd
